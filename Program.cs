57cbd8480b0f2dbf7fa2555e712fb0962c8c8b96
diana427 <72410558+diana427@users.noreply.github.com>
2020-10-30 14:25:56 +0200

    Lab 2

--- Program.cs
+++ Program.cs
@@ -3,53 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ConsoleApp3
+class Cilindr
 {
-    class Program
+    private double radius_osnovania, vysota; // радиус основания и высота
+
+    public double get_radius() //получаем радиус
     {
-        static void Main(string[] args)
-        {
-            float x;
-            string s;
-            Console.WriteLine("Введите x");
+        return radius_osnovania;
+    }
+    public void set_radius(double value_radius) //задаём радиус
+    {
+        radius_osnovania = value_radius;
+    }
+    public double get_vysota() //получаем высоту
+    {
+        return vysota;
+    }
+    public void set_vysota(double value_vysota) //задаём высоту
+    {
+        vysota = value_vysota;
+    }
+    public Cilindr(double r, double h)
+    {
+        radius_osnovania = r;
+        vysota = h;
+    }
+    public double Obyem()
+    {
+        return Math.PI * Math.Pow(radius_osnovania, 2) * vysota;
+    }
+    public void vyvod()
+    {
+        Console.WriteLine("Радиус= {0,5:f3} Высота= {1,5:f3}",
+        radius_osnovania, vysota);
+    }
+  
+}
+class Program
+{
 
-            s = Console.ReadLine();
-            x = Convert.ToSingle(s);
+    static void Main(string[] args)
+    {
+        Cilindr C1 = new Cilindr(4, 12); //перший циліндр
 
-            double p = Math.Sin(x/3) + (Math.Pow(2, x) / Math.Cos(x));
-            
-            Console.WriteLine("1) p = " +p);
+        Console.WriteLine("Програма, яка розраховує об'єм цилiндра: \n");
+        Console.WriteLine("Радiус= {0,5:f3} Висота= {1,5:f3}",
+        C1.get_radius(), C1.get_vysota());
+        Console.WriteLine("Об'єм цилiндра:{0,5:f3}", C1.Obyem());
 
-            p = (Math.Cos(x) / Math.Sin(x)) - Math.Acos(Math.Pow(x, 3));
+        Console.WriteLine("\nВведите радиус основания и высоту третьего цилиндра");
+        Cilindr C2 = new Cilindr(Convert.ToDouble(Console.ReadLine()),
+         Convert.ToDouble(Console.ReadLine()));
+        //радіус і висота другого цилінда
+        C2.vyvod();
+        Console.WriteLine(C2.Obyem());
 
-            Console.WriteLine("2) p = " + p);
-
-            p = Math.Pow(x, x + 3) + Math.Atan(x);
-            Console.WriteLine("3) p = " + p);
-
-            p = 1 + Math.Pow((1 + Math.Log10(x)), 1 / 3);
-            Console.WriteLine("4) p = " + p);
-
-            p = Math.Pow((Math.Cos(x - Math.PI)) / (Math.Sin(x - Math.PI)), -1);
-            Console.WriteLine("5) p = " + p); 
-
-            p = Math.Sinh(Math.Pow(x, 0.5));
-            Console.WriteLine("6) p = " + p);
-
-            p = Math.Pow(x, 2) + 2*x - Math.Tanh(x/2);
-            Console.WriteLine("7) p = " + p);
-
-            p = Math.Log(Math.Log10(Math.Pow(Math.E, x))) - 0.35;
-            Console.WriteLine("8) p = " + p);
-
-            p = Math.Pow(x/2, 1.72) - Math.Tan(Math.PI - 1);
-            Console.WriteLine("9) p = " + p);
-
-            p = Math.Abs(Math.Log(Math.Atan(x)));
-            Console.WriteLine("10) p = " + p);
-            Console.ReadKey();
-        }
+        Console.ReadKey();
     }
 }
+
+
 
